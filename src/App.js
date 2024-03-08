47b28a535b3c71eceac7d20f5ae9a615f60c165f
Manish88879 <manish49724@gmail.com>
2024-03-08 16:39:58 +0530

    SecondCommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Routes, BrowserRouter } from 'react-router-dom';
 //import Home from './Home'; // Import your Home component
 import Splash from './Screens/SplashScreen'; // Import your Splash component
 import RegisterPage from './Screens/AuthScreen/RegisterPage/RegisterPage';
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-        <Route path="/" element={<Splash />} />
+        <Route path="/database" element={<Splash />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/otpPage" element={<OtpPage />} />
         <Route path="/setupPassword" element={<SetupPassword/>} />
